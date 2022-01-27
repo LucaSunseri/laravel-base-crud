947f92a50776d65d351f91a6feaa3e7e009ad7ce
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2022-01-27 15:01:57 +0100

    Database done

---
+++ database/migrations/2022_01_27_134026_create_comics_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title', 200);
+            $table->text('description', 200);
+            $table->string('thumb');
+            $table->decimal('price', 5, 2);
+            $table->string('series', 150);
+            $table->date('sale_date');
+            $table->string('type', 100);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
